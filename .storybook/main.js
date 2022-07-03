e9b7be450c3cc9e40eb9ad569ebbf8e251b6e305
Gabriel <gabriel1997.castro@gmail.com>
2022-07-03 10:42:42 -0300

    Add Task component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    '@storybook/addon-a11y',
   ],
   framework: "@storybook/react",
   core: {
@@ -13,5 +14,14 @@ module.exports = {
   },
   features: {
     interactionsDebugger: true,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
   },
 };
